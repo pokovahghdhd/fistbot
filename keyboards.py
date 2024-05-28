c0855b44d85178ac59d36585f6c30732df08185a
pokovahghdhd <pokova09k9va@gmail.com>
2024-05-28 15:05:30 +0300

    Add files via upload

---
+++ keyboards.py
@@ -0,0 +1,17 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+
+def get_keyboard_1():
+    keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
+    button_1 = KeyboardButton('Отправить фота кота')
+    button_2 = KeyboardButton('Перейти на следующую клавиатуру')
+    keyboard.add(button_1, button_2)
+    return keyboard
+
+
+def get_keyboard_2():
+    keyboard_2 = ReplyKeyboardMarkup(resize_keyboard=True)
+    button_3 = KeyboardButton('Отправить фота собаки')
+    button_4 = KeyboardButton('Вернутьтя на 1 клавиатуру')
+    keyboard_2.add(button_3, button_4)
+    return keyboard_2
